c64f92ef22c757e11d0d40a7fff455d4ac5df496
Hanafi Abdilah <55283082+hanafabdilah@users.noreply.github.com>
2021-02-02 13:42:25 +0700

    Membuat API Endpoint Attendance History

--- app/Http/Controllers/Api/AttendanceController.php
+++ app/Http/Controllers/Api/AttendanceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Traits\ImageStorage;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -100,5 +101,25 @@ class AttendanceController extends Controller
                 Response::HTTP_UNPROCESSABLE_ENTITY
             );
         }
+    }
+
+    public function history(Request $request)
+    {
+        $request->validate([
+            'from' => ['required'],
+            'to' => ['required'],
+        ]);
+
+        $history = $request->user()->attendances()->with('detail')->whereBetween(DB::raw('DATE(created_at)'), [
+            $request->from, $request->to
+        ])->get();
+
+        return response()->json(
+            [
+                'message' => "List of presences by user",
+                'data' => $history,
+            ],
+            Response::HTTP_OK
+        );
     }
 }
